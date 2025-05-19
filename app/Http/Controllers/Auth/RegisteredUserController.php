0fc4277e772e2de3c1a58d25dc6e55c5bc39f531
Andre Firmansyah <166293262+af133@users.noreply.github.com>
2025-05-19 18:09:11 +0700

    Update RegisteredUserController.php

--- app/Http/Controllers/Auth/RegisteredUserController.php
+++ app/Http/Controllers/Auth/RegisteredUserController.php
@@ -42,7 +42,7 @@ class RegisteredUserController extends Controller
             'password' => Hash::make($request->password),
         ]);
 
-        $user->assignRole('admin');
+        $user->assignRole('participant');
 
         event(new Registered($user));
 
